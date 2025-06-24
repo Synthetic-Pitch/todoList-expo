6af526a50634f7bfd670bf5664390fab1b16f5a6
reymark <genuinepower123@gmail.com>
2025-06-25 00:36:50 +0800

    main

--- app/components/headers.tsx
+++ app/components/headers.tsx
@@ -1,55 +1,67 @@
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 
 const Header = () => {
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
     
     const pickImage = async () => {
-    // Request permission
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    
-    if (permissionResult.granted === false) {
-        alert("Permission to access camera roll is required!");
-        return;
-    }
-    
-    // Launch image picker
-    const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ['images'],
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-    });
+        // Request permission
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+        
+        // Launch image picker
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
 
-    // Set selected image
-    if (!result.canceled && result.assets && result.assets.length > 0) {
-        setSelectedImage(result.assets[0].uri);
-    }
+        // Set selected image
+        if (!result.canceled && result.assets && result.assets.length > 0) {
+            setSelectedImage(result.assets[0].uri);
+        }
     };
+
     return (
-        <View className='h-[200px] bg-[#aaaae9] justify-center px-8'>
-            
-            {selectedImage ? (
-            <View className='mt-4'>
-                <Image 
-                source={{ uri: selectedImage }}
-                className='w-[100px] h-[100px] rounded-lg'
-                resizeMode="cover"
-                style={{
-                    borderRadius:100,
-                    
-                }}
-                />
+        <View className='h-[200px] bg-[#97B9DF] px-8 rounded-br-[50px] flex-row items-center gap-6'>
+            { selectedImage ? (
+                <View className='mt-4 relative justify-center h-[100px]'>
+                    <Image 
+                        source={{ uri: selectedImage }}
+                        className='w-[100px] h-[100px] rounded-lg'
+                        resizeMode="cover"
+                        style={{
+                            borderRadius:100,zIndex:1
+                        }}
+                    />
+                    <Pressable onPress={pickImage} className='absolute bottom-0 bg-[#97B9DF] z-10 rounded-md pr-1'>
+                        <Image
+                            source={require('../../assets/images/camera.png')}
+                            className='z-10 h-7 w-7'
+                        />
+                    </Pressable>
+                </View>
+                ):(
+                    <Pressable 
+                        onPress={pickImage} 
+                        className='h-[100px] w-[100px] bg-[#d0d0d0] rounded-full items-center justify-center'
+                    >
+                        <Text>import</Text>
+                    </Pressable>
+                )
+            }
+
+            <View>
+                <Text className='text-xl font-bold'>Marked</Text>
+                <Text className='text-xl font-bold text-white'>{100}pts</Text>
             </View>
-            ):(
-            <TouchableOpacity 
-                onPress={pickImage} 
-                className='h-[100px] w-[100px] bg-[#d0d0d0] rounded-full items-center justify-center'
-            >
-                <Text>import</Text>
-            </TouchableOpacity>
-            )}
+
         </View>
     )
 }
