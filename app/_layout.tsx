10027823b18e3525d6bfca422bc8bdf05089b3a4
reymark <genuinepower123@gmail.com>
2025-06-24 23:48:24 +0800

    hello

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <React.Fragment>
+      <StatusBar style="dark"/>
+      <Slot />
+    </React.Fragment>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,53 @@
-import { Text, View } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
-export default function Index() {
+const Page = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // Request permission
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    // Launch image picker
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // Set selected image
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View
+    <View className='h-[200px] bg-[#aaaae9] justify-center items-center'>
+      <TouchableOpacity 
+        onPress={pickImage} 
+        className='h-20 w-20 bg-[#d0d0d0] justify-center items-center rounded-lg'
+      >
+        <Text>Pick Image</Text>
+      </TouchableOpacity>
       
-    >
-      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+      {selectedImage && (
+        <View className='mt-4'>
+          <Image 
+            source={{ uri: selectedImage }}
+            className='w-24 h-24 rounded-lg'
+            resizeMode="cover"
+          />
+        </View>
+      )}
     </View>
-  );
+  )
 }
+
+export default Page;
