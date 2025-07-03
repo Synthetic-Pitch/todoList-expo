cc7cb1b7d67fb62773f23f813f4a59054960fc42
reymark <genuinepower123@gmail.com>
2025-07-03 12:55:52 +0800

    main

--- app/createTodo/index.tsx
+++ app/createTodo/index.tsx
@@ -1,9 +1,68 @@
-import { Text, View } from 'react-native'
+import Header from '@/components/headers';
+import { Dimensions, Pressable, Text, TextInput, View } from "react-native";
+import { Shadow } from "react-native-shadow-2";
+const screenWidth = Dimensions.get("window").width;
 
 const Page = () => {
   return (
-    <View>
-        <Text>asd</Text>
+   <View className="flex-1 bg-[#E5E5E5] relative">
+      <Shadow
+        startColor="rgb(189, 189, 189)"
+        endColor="rgb(189, 189, 189,.1)"
+        safeRender
+        distance={8}
+        style={{position:"relative",zIndex:20,marginBottom:10,borderBottomRightRadius:53,width:screenWidth}}
+      >
+        <Header/>
+      </Shadow>
+      <View className="flex-1 relative"> 
+        
+        <View className='relative w-full items-center h-[calc(100%-44.8px) mt-4'>
+          <Shadow
+            offset={[0,3]}
+            distance={6}
+            >
+            <TextInput
+              multiline
+              placeholder='todo text'
+              style={{
+                height:200,
+                width:screenWidth-40,
+                paddingVertical:19,
+                paddingHorizontal:19,
+                borderRadius:23,
+                textAlignVertical:'top',
+                backgroundColor:"#E5E5E5",
+                alignSelf:'center',
+                fontSize:18,
+              }}
+            />
+          </Shadow>
+        </View>
+    
+
+        <View  className='items-end px-6 justify-center mt-6'>
+          <Pressable className='bg-[#97B9DF] py-2 px-8 rounded-2xl'>
+            <Text className='text-xl font-poppins text-white'>add</Text>
+          </Pressable>
+        </View>
+
+        <View className='px-4'>
+          <Text className='font-poppins text-[gray]'>color</Text>
+          <View className='flex-row gap-2'>
+              <Pressable className='bg-[#DFCCA7] h-10 w-10 rounded-full'/>
+              <Pressable className='bg-[#F2BBEE] h-10 w-10 rounded-full'/>
+              <Pressable className='bg-[#F2BBBC] h-10 w-10 rounded-full'/>
+              <Pressable className='bg-[#BFF2BB] h-10 w-10 rounded-full'/>
+              <Pressable className='bg-[#F2D6BB] h-10 w-10 rounded-full'/>
+          </View>
+        </View>
+
+        <View
+          className='h-[44.8px] w-full bg-black absolute bottom-0'
+        >
+        </View>
+      </View>
     </View>
   )
 }
--- components/mapItem.tsx
+++ components/mapItem.tsx
@@ -1,33 +1,36 @@
-import UseCrudHooks from '@/hooks/useCrudHooks';
+import useCrudHooks from '@/hooks/useCrudHooks';
 import { Link } from 'expo-router';
-import { Pressable, ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 const MapItem = () => {
-    const {createData,readData,resetData,todo} = UseCrudHooks();
+    const {createData,readData,resetData,todo} = useCrudHooks();
 
     return (
         <View  className="h-[93.3%] bg-[#E5E5E5] gap-2 relative">
             <ScrollView
                 className='flex-1 relative'
             >
-                {
+                {   
+                    Array.isArray(todo) &&
                     todo.map((todos) =>
-                        <View key={todos.key} className='bg-[#f7c5c5] w-[93%] h-[80px] m-auto mb-2 rounded-3xl'>
+                        <View 
+                            key={todos.key} 
+                            className='bg-[#f7c5c5] w-[93%] h-[80px] m-auto mb-2 rounded-3xl'>
                             <Text>{todos.todo}</Text>
                         </View>
                     )
                 }
             </ScrollView>
-            <Pressable onPress={readData}><Text>get</Text></Pressable>
-            <Pressable onPress={resetData}><Text>reset</Text></Pressable>
             
-            <View className='absolute bottom-0 left-0 my-4 w-full items-end px-4'>
-                <Link
-                     href={"/createTodo" as any}
-                    className='bg-[#97B9DF] w-[60px] h-[60px] rounded-full items-center justify-center'
-                >
-                    <Text className='text-3xl'>+</Text>
-                </Link>
+            <View className='absolute bottom-0 left-0 my-4 w-full items-end px-4 '>
+                <View className='bg-[#97B9DF] w-[60px] h-[60px] items-center justify-center rounded-full'>
+                    <Link
+                        href={"/createTodo" as any}
+                       className='inline-block'
+                    >
+                        <Text className='text-3xl'>+</Text>
+                    </Link>
+                </View>
             </View>
         </View>
     )
